97ad5193c7ea7813f2cf26e64b00329410952b36
williamhlane <williamhlane3@gmail.com>
2021-04-25 15:53:28 -0700

    In this push, I have added a javascript image view that is undone, improved styling but text still needed styling.

--- js/script.js
+++ js/script.js
@@ -7,20 +7,27 @@ let showSearchResults = () => {
     .then((res) => {
       return res.json()
     }).then((json) => {
-     // console.log(json + json.data + json.data[0] + json.data[0].images);
+      //console.log(json + json.data + json.data[0] + json.data[0].images);
+      console.log(json.data);
       let spans = "";
       for (let ri of json.data) {
         let imgUrl = ri.images.original.url;
-        spans += "<span><img src=" + '"' + imgUrl + '"' + " /> </span>";
-        console.log(imgUrl + spans);
-        //undefined
+        let previewUrl = ri.images.preview_gif.url;
+
+        spans += "<span>" + '<a onclick="'+ "showFullImage('" + imgUrl + "')" + '">' +"<img src=" + '"' 
+        + previewUrl + '"' + " /> </span></a>";
+        console.log(spans);
       }
       let spansDiv = document.getElementsByTagName('div');
       spansDiv[1].innerHTML = spans;
     })
-
-
-
-
-
 }
+let showFullImage = (imgUrl) => {
+  document.getElementById("imageViewer").style.display = "block";
+  document.getElementById("fullSizeImg").src = imgUrl;
+  
+  
+}
+let closeImageViewer = () => {
+  document.getElementById("imageViewer").style.display = "none";
+} 
