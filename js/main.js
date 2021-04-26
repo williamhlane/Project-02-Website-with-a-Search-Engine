7b81e2c257e78d27eab5d0ffca6943aa44528157
williamhlane <williamhlane3@gmail.com>
2021-04-25 19:16:06 -0700

    In this push, I have added a javascript image view that is undone, improved text styling and added styling for 320 pixels or under.

--- js/main.js
+++ js/main.js
@@ -8,26 +8,33 @@ let showSearchResults = () => {
       return res.json()
     }).then((json) => {
       //console.log(json + json.data + json.data[0] + json.data[0].images);
-      console.log(json.data);
+      //console.log(json.data);
       let spans = "";
+      let timesRun = 0;
       for (let ri of json.data) {
+        timesRun += 1;
         let imgUrl = ri.images.original.url;
         let previewUrl = ri.images.preview_gif.url;
 
-        spans += "<span>" + '<a onclick="'+ "showFullImage('" + imgUrl + "')" + '">' +"<img src=" + '"' 
-        + previewUrl + '"' + " /> </span></a>";
-        console.log(spans);
+        spans += "<span>" + '<a onclick="' + "showFullImage('" + imgUrl + "')" + '">' + "<img src=" + '"'
+          + previewUrl + '"' + " /> </span></a>";
+        //console.log(spans);
       }
+      console.log(timesRun);
       let spansDiv = document.getElementsByTagName('div');
       spansDiv[1].innerHTML = spans;
+      if (timesRun == 0) {
+        spansDiv[1].innerHTML = "<div><h1>No results found.</h1></div>";
+      }
     })
 }
 let showFullImage = (imgUrl) => {
   document.getElementById("imageViewer").style.display = "block";
   document.getElementById("fullSizeImg").src = imgUrl;
-  
-  
+
+
 }
 let closeImageViewer = () => {
   document.getElementById("imageViewer").style.display = "none";
-} 
+  
+}
